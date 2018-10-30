efaf8d099e566d463a9168699962ff7232a5e678
Matthew Ming <34111403+DevMMing@users.noreply.github.com>
2018-10-30 03:36:03 -0400

    fixed randomfile.c

--- randomfile.c
+++ randomfile.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<fcntl.h>
-int main(void) {
-  int *c = malloc(sizeof(char));
-  int* arr=malloc(sizeof(int));
+int randNum(){
+  int c;
   int file = open("/dev/random",O_RDONLY);
-  int something = read("/dev/random",c,10);
-  printf("Hello World %d\n",c);
-  return 0;
+  read(file,&c,sizeof(c));
+  close(file);
+  return c;
 }
-int* randomarr(){
-  int* arr=malloc(sizeof(int));
-  int file = open("/dev/random",O_RDONLY);
-  return arr;
-
+int main(void) {
+  int arr[10];
+  for(int i=0;i<10;i++){
+    arr[i]=randNum();
+    printf("What is arr[%d]??:%d\n",i,arr[i]);
+  }
+  int file=open("file.txt",O_WRONLY | O_CREAT);
+  write(arr,sizeof(int),sizeof(arr),file);
+  close(file);
+  int file2=open("file.txt",O_RDONLY);
+  read(arr,sizeof(int),sizeof(arr),file2);
+  close(file2);
+  return 0;
 }
